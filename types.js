b6a62bf2bbdb5b0094b331c14b1d522606a69220
Sagor0sud <mehedihasan74364@gmail.com>
2025-10-05 01:12:57 +0600

    my21

---
+++ types.js
@@ -0,0 +1,16 @@
+var name = "Mehedi Hasan";
+var address ="17 No Rup Chane Das Line"
+var country ='Bangladesh';
+var Isit =true;
+var notAvailable = false;
+var carbon = null;
+var orangePrice=50;
+var applePrice=80;
+console.log(typeof name);
+console.log(typeof address);
+console.log(typeof country);
+console.log(typeof Isit);
+console.log(typeof notAvailable);
+console.log(typeof carbon);
+console.log(typeof orangePrice);
+console.log(typeof applePrice);
